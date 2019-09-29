ffb38cb784d243d57d90d1e38b7257f34a95c5a2
Sergei <sergey0034@mail.ua>
2019-09-29 23:04:35 +0300

    4.0

--- components/Search/ModelSearch.js
+++ components/Search/ModelSearch.js
@@ -1,10 +1,15 @@
 export class ModelSearch {
-  constructor(controller) {
-    this.controller = controller;
+  constructor(router) {
+    this.router = router;
   }
 
   filterProducts(phrase) {
     const products = JSON.parse(localStorage.getItem('products'));
     console.log('2', phrase);
+
+    const filteredProducts = products
+      .filter(prod => prod.name.toLowerCase().startsWith(phrase.toLowerCase()));
+
+    this.router.router.controllerProduct.sendProductsToRender(filteredProducts); // ?????????????
   }
 }
--- components/ServiceProduct.js
+++ components/ServiceProduct.js
@@ -1,7 +1,8 @@
 export class ServiceProduct {
 
   getProducts() {
-    return fetch('./data/goods.json')
+    // return fetch('./data/goods.json')
+    return fetch('https://cors-anywhere.herokuapp.com/https://gentle-coast-49966.herokuapp.com/get-goods')
       .then(data => data.json())
       .then(data => {
         if (localStorage.getItem.products) {
