2f1133b231695022844f09ac963deac38d607e0f
Sergei <sergey0034@mail.ua>
2019-09-29 19:50:18 +0300

    v2.1

--- components/Category/ModelCategory.js
+++ components/Category/ModelCategory.js
@@ -15,20 +15,3 @@ export class ModelCategory {
     }
   }
 }
-
-// export class ModelCategory {
-// constructor(router) {
-//   this.router = router;
-
-  // getCategory(e) {
-  //   const category = e.target.parentNode.id;
-  //   let data = this.router.router.serviceProduct.allProducts;
-
-  //   if (category === 'all') {
-  //     this.router.router.controllerProduct.sendProductsToRender(data);
-  //   } else {
-  //     let dataFiltered = data.filter((el) => el.type === category);
-  //     this.router.router.controllerProduct.sendProductsToRender(dataFiltered);
-  //   }
-  // }
-// }
