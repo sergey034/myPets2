64813eaecac93f446e9a1990fd76c57498b109ff
Sergei <sergey0034@mail.ua>
2019-09-29 20:52:43 +0300

    v2.2.2

--- components/Category/ViewCategory.js
+++ components/Category/ViewCategory.js
@@ -1,8 +1,13 @@
 export class ViewCategory {
   constructor(contr) {
     this.controller = contr;
-    this.output = document.querySelector('#products');
+    document.addEventListener('DOMContentLoaded', this.renderNavbar);
     this.navbar = document.querySelector('#main-menu');
     this.navbar.addEventListener('click', this.controller.runGetCategory.bind(this.controller));
+    this.output = document.querySelector('#products');
+  }
+
+  renderNavbar() {
+    // document.querySelector('#jjj').innerHTML = this.templater.load(product, this.output);
   }
 }
